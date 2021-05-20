a0657e7cdb28cafb6bc27b1cbba625b4951aaf15
huangwei <harw.op@qq.com>
2021-05-21 00:37:48 +0800

    initial setup

--- Swift-RogueLike/main.swift
+++ Swift-RogueLike/main.swift
@@ -7,5 +7,37 @@
 
 import Foundation
 
-print("Hello, World!")
+let tcod = TCOD()
+let title: String = "Swift Rogue Like Tutorial"
+let width: Int32 = 80
+let height: Int32 = 40
+
+tcod.initRoot(w: width, h: height, title: title)
+tcod.consoleClear()
+
+var playerx: Int32 = width / 2
+var playery: Int32 = height / 2
+tcod.putChar(x: playerx, y: playery, char: "@")
+tcod.flush()
+
+while (tcod.windowisClosed() == false) {
+    guard tcod.chkKeypress() else { continue }
+    
+    switch (tcod.key.vk) {
+    case TCODK_UP:
+        playery -= 1
+    case TCODK_DOWN:
+        playery += 1
+    case TCODK_LEFT:
+        playerx -= 1
+    case TCODK_RIGHT:
+        playerx += 1
+    default:
+        continue
+    }
+    
+    tcod.consoleClear()
+    tcod.putChar(x: playerx, y: playery, char: "@")
+    tcod.flush()
+}
 
